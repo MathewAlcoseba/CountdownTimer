a601bb130a4e6ceadc19dd868a8728c7292897cc
MathewAlcoseba <mathewalcoseba25@gmail.com>
2023-10-17 11:11:31 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class CountdownTimerDemo extends StatefulWidget {
+  @override
+  _CountdownTimerDemoState createState() => _CountdownTimerDemoState();
+}
+
+class _CountdownTimerDemoState extends State<CountdownTimerDemo> {
+  Timer? countdownTimer;
+  Duration myDuration = Duration(days: 5);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void startTimer() {
+    countdownTimer =
+        Timer.periodic(Duration(seconds: 1), (_) => setCountDown());
+  }
+
+  void stopTimer() {
+    countdownTimer?.cancel();
+  }
+
+  void resetTimer() {
+    stopTimer();
+    setState(() {
+      myDuration = Duration(days: 5);
+    });
+  }
+
+  void setCountDown() {
+    final reduceSecondsBy = 1;
+    setState(() {
+      final seconds = myDuration.inSeconds - reduceSecondsBy;
+      if (seconds < 0) {
+        countdownTimer?.cancel();
+      } else {
+        myDuration = Duration(seconds: seconds);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String strDigits(int n) => n.toString().padLeft(2, '0');
+    final days = strDigits(myDuration.inDays);
+    final hours = strDigits(myDuration.inHours.remainder(24));
+    final minutes = strDigits(myDuration.inMinutes.remainder(60));
+    final seconds = strDigits(myDuration.inSeconds.remainder(60));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Countdown Timer Demo'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            Text(
+              '$days:$hours:$minutes:$seconds',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 50,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: startTimer,
+              child: Text(
+                'Start',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (countdownTimer != null && countdownTimer!.isActive) {
+                  stopTimer();
+                }
+              },
+              child: Text(
+                'Stop',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                resetTimer();
+              },
+              child: Text(
+                'Reset',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() => runApp(MaterialApp(
+      home: CountdownTimerDemo(),
+    ));
